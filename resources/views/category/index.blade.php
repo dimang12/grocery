6c6fefc6512fffddc134fc31472a964e5c203204
dimangchou <dimang12@gmail.com>
2021-10-15 22:42:13 -0700

    adjust style at home category page to look more equaly

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,18 +1,21 @@
 @extends('layouts.app')
 
-@section('title')
-Main category
-@endsection
+@section('title','- Main category')
+
 
 @section('content')
 <ul class="row">
+    <li class="col-12  list-unstyled">
+        <h3 class="title pb-lg-4 mb-lg-4 border-bottom">
+            Main Sections
+        </h3>
+    </li>
     @foreach ($categories as $cate)
     <li class="list-unstyled col-3">
-        <a href="{{ route('category')}}" class="btn btn-outline-primary w-100">
+        <a href="{{route('category')}}/{{$cate->id}}" class="btn btn-outline-primary w-100">
             {{ $cate->category_name}}
         </a>
     </li>
     @endforeach
-
 </ul>
 @endsection
